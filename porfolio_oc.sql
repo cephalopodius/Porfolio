7d08c5d0d0fd665bd8159614c729bc06d7313833
Unknown <campino.robin@hotmail.fr>
2019-05-01 16:52:14 +0200

    new sql file

--- porfolio_oc.sql
+++ porfolio_oc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 29 avr. 2019 à 13:18
+-- Généré le :  mer. 01 mai 2019 à 14:51
 -- Version du serveur :  5.7.19
 -- Version de PHP :  7.1.9
 
@@ -58,18 +58,17 @@ CREATE TABLE IF NOT EXISTS `blog` (
   `image` varchar(50) NOT NULL,
   PRIMARY KEY (`id_Blog`),
   KEY `id_Admin` (`id_Admin`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `blog`
 --
 
 INSERT INTO `blog` (`id_Blog`, `Titre`, `date`, `Chapo`, `Contenu`, `id_Admin`, `image`) VALUES
-(2, 'TRM', '2019-04-24 00:38:53.000000', 'Site vitrine pour TRM', 'Site réalisé pour TRM au sein de la Société Française de Développement Numérique (SFDN). https://trm53.fr', 1, 'asset/img/project/TRM.png'),
-(3, 'Fastauto', '2019-04-23 17:01:21.000000', 'Site d\'annonce de voiture ', 'Site réalisé en tant que freelance pour un particulier. http://fast-auto22.fr/', 1, 'asset/img/project/fastauto.png'),
-(4, 'Taxi Courapied', '2019-04-23 16:42:36.000000', 'Un site vitrine pour taxi', 'Site réalisé au sein de la SFDN  dans mes débuts. Le projet fut entièrement mené par mes soins , du cahier des charges à la conception. http://taxicourapied.fr/', 1, 'asset/img/project/taxicourapied.png'),
-(12, 'Docteur Game', '2019-04-23 16:50:19.000000', 'Un site promotteur de jeux mobile', 'J\'ai travaillé sur la conception de ce site au sein d\'une petite équipe.  Il fut réalisé avec le framework polymer 1  de google. http://drgames.fr/fr/', 1, 'asset/img/project/drGame.png'),
-(13, 'dsds', '2019-04-29 14:58:23.000000', 'dsds', 'dsds', 1, 'asset/img/project/cabin.png');
+(2, 'TRM !', '2019-04-29 21:39:19.000000', 'Site vitrine pour TRM', 'Site réalisé pour TRM au sein de la Société Française de Développement Numérique (SFDN). https://trm53.fr', 1, 'asset/img/project/TRM.png'),
+(3, 'Fastauto', '2019-04-29 17:55:52.000000', 'Site d\'annonce de voiture ', 'Site réalisé en tant que freelance pour un particulier vendeur de voiture d\'occasion. ', 1, 'asset/img/project/fastauto.png'),
+(4, 'Taxi Courapied !', '2019-04-29 20:02:30.000000', 'Un site vitrine pour taxi', 'Site réalisé au sein de la SFDN  dans mes débuts. Le projet fut entièrement mené par mes soins , du cahier des charges à la conception. http://taxicourapied.fr/', 1, 'asset/img/project/taxicourapied.png'),
+(12, 'Docteur Game', '2019-04-23 16:50:19.000000', 'Un site promotteur de jeux mobile', 'J\'ai travaillé sur la conception de ce site au sein d\'une petite équipe.  Il fut réalisé avec le framework polymer 1  de google. http://drgames.fr/fr/', 1, 'asset/img/project/drGame.png');
 
 -- --------------------------------------------------------
 
@@ -88,33 +87,26 @@ CREATE TABLE IF NOT EXISTS `commentaire` (
   PRIMARY KEY (`id_Com`),
   KEY `id_User` (`id_User`),
   KEY `id_Blog` (`id_Blog`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `commentaire`
 --
 
 INSERT INTO `commentaire` (`id_Com`, `Datecom`, `TextCom`, `Validation`, `id_User`, `id_Blog`) VALUES
-(1, '2018-10-26 16:51:31.000000', 'premier commentaire', 1, 1, 2),
 (2, '2018-10-30 01:52:44.000000', 'testadd com', 1, 1, 2),
-(3, '2018-10-30 22:08:42.000000', 'testest', 0, 1, 2),
-(4, '2018-11-06 19:52:41.000000', 'youpilala', 0, 1, 3),
 (5, '2018-11-06 19:56:07.000000', 'test dans jolie boite', 1, 1, 3),
-(6, '2018-11-13 03:15:29.000000', 'test de commentaire', 0, 1, 2),
 (7, '2019-02-18 18:19:13.000000', 'blah', 1, 1, 2),
-(8, '2019-02-21 21:03:16.000000', 'blahblah', 0, 1, 26),
-(9, '2019-02-22 02:52:57.000000', 'brouah', 1, 10, 2),
-(10, '2019-02-22 03:08:48.000000', 'trou', 0, 11, 3),
-(11, '2019-02-22 03:10:09.000000', 'hehe', 0, 13, 2),
-(12, '2019-02-22 03:10:21.000000', 'test', 0, 13, 3),
-(13, '2019-02-26 12:42:52.000000', 'brululululululu', 0, 1, 9),
-(14, '2019-02-26 12:45:11.000000', 'tetouille', 0, 14, 9),
-(15, '2019-02-26 16:58:36.000000', 'fuck', 0, 1, 10),
-(16, '2019-03-12 15:41:20.000000', 'truytruy', 0, 1, 3),
-(17, '2019-03-12 20:12:24.000000', 'treui', 0, 1, 2),
-(18, '2019-03-14 01:50:19.000000', 'psr2', 0, 1, 4),
-(19, '2019-04-22 14:51:09.000000', 'gfgdfbdbf', 0, 1, 12),
-(20, '2019-04-22 14:52:52.000000', 'poil', 0, 1, 12);
+(16, '2019-03-12 15:41:20.000000', 'truytruy', 1, 1, 3),
+(17, '2019-03-12 20:12:24.000000', 'treui', 1, 1, 2),
+(21, '2019-04-29 18:11:55.000000', 'Bonjour je test les commentaire', 1, 1, 3),
+(22, '2019-04-29 19:10:53.000000', 'beau taxi', 1, 32, 4),
+(23, '2019-04-29 19:30:12.000000', 'bouyabouya', 0, 1, 2),
+(24, '2019-04-29 19:30:54.000000', 'vroomvroom', 1, 32, 3),
+(25, '2019-04-29 19:35:04.000000', 'miam', 0, 32, 3),
+(26, '2019-04-29 21:38:33.000000', 'test', 1, 33, 3),
+(28, '2019-05-01 16:44:31.000000', 'vroom', 1, 37, 3),
+(29, '2019-05-01 16:48:56.000000', 'truite', 1, 43, 3);
 
 -- --------------------------------------------------------
 
@@ -133,25 +125,21 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id_User`),
   UNIQUE KEY `Mail` (`Mail`),
   KEY `id_Admin` (`id_Admin`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`id_User`, `prenom`, `nom`, `id_Admin`, `Mail`, `Password`) VALUES
-(1, 'Robin', 'Campino', 1, 'campino.robin@hotmail.fr', 'kraken333'),
-(2, 'Rabbit', 'Roger', NULL, 'Rabbit@hotmail.fr', 'carotte'),
-(3, 'Plombier', 'Mario', NULL, 'Mario@hotmail.fr', 'peach'),
-(4, 'Le dernier', 'Denver', NULL, 'dinosaure@hotmail.fr', 'denver'),
-(5, 'test', 'test', NULL, 'test@test.te', 'test4'),
-(13, 'wizard', 'wizard', NULL, 'wizard@wizard.wizard', 'wizard'),
-(22, 'sd', 'sd', NULL, 'camfgfffpino.robin@hotmail.fr', 'kraken333'),
-(23, 'sta', 'cru', NULL, 'cer@cer.cer', 'cer'),
-(24, 'tyy', 'ty', NULL, 'tyy@ty.ty', 'ty'),
-(25, 'ounet', 'testounet', NULL, 'cao.robin@hotmail.fr', 'kraken333'),
-(27, 'sqsf', 'juve', NULL, 'fdfdfd@fdfd.ffff', 'fdfdf'),
-(31, 'lala', 'tralala', NULL, 'campino.robin@hotmail.', 'kraken333');
+(1, 'robin', 'campino', 1, 'campino.robin@hotmail.fr', '$2y$12$Gv1FdWmmQk7DUffmaASbbe/1Mow.e5dR/WcLj3K7sXBTomqiZWZzy'),
+(34, 'jesus', 'jesus', NULL, 'jesus@revient.sien', '$2y$12$AYX6OyDPcTbIrNEAy7Ohw.P8FqLw6igmVteQe3KAtCsrEC/kFKLna'),
+(35, 'roger', 'roger', NULL, 'roger@rabit.com', '$2y$12$GBXH3UwCFImvtdiJtJZf3em5r8Z6.fN4mTM/tDtYcKTjPUsqcJ5mi'),
+(36, 'ty', 'ty', NULL, 'ty@ty.ty', '$2y$12$HhuA3pOVTFnrfbX9pd/EguN1aRXAY0ztkZiQplhkE2f/foCAFNDmu'),
+(37, 'doom', 'doom', NULL, 'doom@doom.doom', '$2y$12$mnTXOexA9apqEmPPMeBR..QrOrTPg/Y0mFryzhns2XbwGyU4QorLy'),
+(38, 'ra', 'ra', NULL, 'ra@ra.ra', '$2y$12$Wr5G6Oct/ib75.dLr2TtNu9s4n1aYvB3CUN0itSB6apNnKrgdid2K'),
+(42, 'ru', 'ru', NULL, 'ru@ru.ru', '$2y$12$6AsaVJwH9PifTM5IweQATecuh0smR5I6ttEirkR6p0xzc2PwbNNHm'),
+(43, 'rez', 'rez', NULL, 'rez@rez.rez', '$2y$12$Iq0/lTBxb63c12ClT0rMHeFA3K8SCkDjnmfCTugZpmnu1XjDqljh2');
 
 --
 -- Contraintes pour les tables déchargées
